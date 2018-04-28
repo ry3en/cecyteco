31cb4128227e123de676520e2bc6e66e31042cd7
joselopez <antrax_998@me.com>
2018-04-28 09:06:24 -0500

    lol

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,:omniauthable,
          :omniauth_providers =>[:facebook]
 
-validates :username, presence: true, uniqueness: true, length: {in: 3..10}
-validate :validate_username_regex
-
-private
-  def validate_username_regex
-    unless username =~/\A[a-zA-Z]*[a-zA-Z][a-zA-Z0-9_]*\z/
-      errors.add(username,"El username debe iniciar con una letra")
-    end
-  end
+         validates :username, presence: true,uniqueness: true,length: {in: 3..12}
+         
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it { should validate_presence_of :username }
-
-  it "should work" do
-    expect(20).to be >10
-  end
+  it { should validate_uniqueness_of :username}
+  it { should validate_length_of(:username).is_at_least(3).is_at_most(12) }
 
 end
