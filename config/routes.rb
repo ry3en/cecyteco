1d69070d4b7438a9579b445f1ad05964e6d0ee7c
joselopez <antrax_998@me.com>
2018-05-01 12:09:58 -0500

    lol

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-resources :articles
-resources :usuarios, as: :users
 
   devise_for :users, controllers:{
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
+resources :articles;
+
   authenticated :user do
     root 'main#home'
   end
+
   unauthenticated :user do
-    root 'main#unregistered'
+    get 'devise/sessions/new' => 'devise/sessions#new'
   end
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
