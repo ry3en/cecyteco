4e90007c8dcfeb021db5bd640183d4693059843c
Gerardo Barragan <davidmaster99.gdm@gmail.com>
2018-05-05 09:14:27 -0500

    lol

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
 
+  resources :articles;
   resources :posts
-  devise_for :users, controllers:{
-    omniauth_callbacks: "users/omniauth_callbacks"
-  }
 
-resources :articles;
+  devise_for :users, controllers:
+  {omniauth_callbacks: "users/omniauth_callbacks"}
 
   authenticated :user do
     root 'main#home'
   end
-
   unauthenticated :user do
     root 'main#unregistered'
   end
+
 end
